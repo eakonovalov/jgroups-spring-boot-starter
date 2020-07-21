6ad537987fbfcba95f049e693cac04a383d91ad0
Evgeny Konovalov <eakonovalov@gmail.com>
2020-07-21 11:49:18 -0400

    Initial commit

---
+++ src/main/java/com/github/eakonovalov/JGroupsClusterAutoConfiguration.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.eakonovalov;
+
+import org.jgroups.JChannel;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Evgeny Konovalov
+ */
+@Configuration
+@ConditionalOnClass(name = "org.jgroups.JChannel")
+@ConditionalOnProperty(value = "jgroups.cluster.enabled", havingValue = "true", matchIfMissing = true)
+@EnableConfigurationProperties(JGroupsClusterProperties.class)
+public class JGroupsClusterAutoConfiguration {
+
+    private final JGroupsClusterProperties cluster;
+
+    public JGroupsClusterAutoConfiguration(JGroupsClusterProperties cluster) {
+        this.cluster = cluster;
+    }
+
+    @Bean(destroyMethod = "close")
+    public JChannel channel() throws Exception {
+        final JChannel channel = new JChannel();
+        channel.connect(cluster.getName() != null ? cluster.getName() : "default");
+
+        return channel;
+    }
+}
---
+++ src/main/java/com/github/eakonovalov/JGroupsClusterProperties.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.eakonovalov;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author Evgeny Konovalov
+ */
+@ConfigurationProperties(prefix = "jgroups.cluster")
+public class JGroupsClusterProperties {
+
+    private Boolean enabled = true;
+    private String name;
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
